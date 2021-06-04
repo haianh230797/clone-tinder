b9ec08c0d304cbd768dc6033249885692f796459
Nguyen Hai Anh <haianh230797@gmail.com>
2021-06-04 17:10:46 +0700

    add API data

--- src/routes/index.js
+++ src/routes/index.js
@@ -24,7 +24,7 @@ export default function TopTap() {
             tabBarLabel={false}
             indicatorStyle={{ backgroundColor: 'red' }}>
             <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Settings" component={Favorite} />
+            <Tab.Screen name="Favorite" component={Favorite} />
         </Tab.Navigator>
     );
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import {
     SafeAreaView,
     ScrollView,
@@ -14,15 +14,33 @@ import {
 } from 'react-native';
 import Carousel from 'react-native-snap-carousel';
 import CardStack, { Card } from 'react-native-card-stack-swiper';
+import normalize from 'react-native-normalize';
+import Colors from '@components/Colors';
+import { Icon } from 'native-base';
 
-const WIDTH_PANDER = Dimensions.get('window').width / 1.25;
-const HEIGHT_PANDER = Dimensions.get('window').height / 2;
+const BASE_LINK = 'https://randomuser.me/api/0.4/?randomapi';
 
 export default function Home() {
-    const [data, setData] = useState([{ title: 1 }, { title: 2 }]);
+    const [dataUser, setDataUser] = useState([]);
+
+    useEffect(() => {
+        getData();
+    }, [getData]);
+
+    const getData = useCallback(() => {
+        fetch(BASE_LINK)
+            .then((response) => response.json())
+            .then((data) => {
+                let arr = [...dataUser];
+                arr.push(data.results[0].user);
+                setDataUser(arr);
+            })
+            .catch((err) => console.log('err when get data: ', err));
+    }, [dataUser]);
 
     return (
         <View style={{ flex: 1 }}>
+            <View style={styles.headerBox} />
             <CardStack
                 style={styles.content}
                 renderNoMoreCards={() => (
@@ -35,20 +53,61 @@ export default function Home() {
                         No more cards :(
                     </Text>
                 )}
-                onSwiped={() => console.log('onSwiped')}
-                onSwipedLeft={() => {
-                    let arr = [...data];
-                    arr.push({ title: Math.floor(Math.random() * 100) });
-                    setData(arr);
-                }}
-                onSwipedRight={() => {
-                    let arr = [...data];
-                    arr.push({ title: Math.floor(Math.random() * 100) });
-                    setData(arr);
-                }}>
-                {data.map((item) => (
-                    <Card style={[styles.card, styles.card1]}>
-                        <Text style={styles.label}>{item.title}</Text>
+                onSwiped={() => getData()}>
+                {dataUser.map((item) => (
+                    <Card
+                        style={styles.card}
+                        onSwipedLeft={() => {}}
+                        onSwipedRight={() => {
+                            console.log(item);
+                        }}>
+                        <View>
+                            <View style={styles.topBox} />
+                            <View style={styles.bottomBox}>
+                                <View style={styles.addressBox}>
+                                    <Text style={styles.addressTitle}>
+                                        My address is{' '}
+                                    </Text>
+                                    <Text style={styles.addressContent}>
+                                        {item.location.street}
+                                    </Text>
+                                </View>
+                                <View style={styles.listFeature}>
+                                    <TouchableOpacity>
+                                        <Icon
+                                            name="user-circle"
+                                            type="FontAwesome"
+                                        />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity>
+                                        <Icon name="calendar" type="Entypo" />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity>
+                                        <Icon
+                                            name="location-outline"
+                                            type="Ionicons"
+                                        />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity>
+                                        <Icon
+                                            name="call-outline"
+                                            type="Ionicons"
+                                        />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity>
+                                        <Icon name="unlocked" type="Fontisto" />
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                            <View style={styles.stretchBox}>
+                                <Image
+                                    style={styles.stretch}
+                                    source={{
+                                        uri: item.picture,
+                                    }}
+                                />
+                            </View>
+                        </View>
                     </Card>
                 ))}
             </CardStack>
@@ -60,83 +119,74 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: 'column',
-        backgroundColor: '#f2f2f2',
     },
+
     content: {
         flex: 5,
         alignItems: 'center',
         justifyContent: 'center',
+        backgroundColor: Colors.ATHENS_GRAY,
+    },
+    headerBox: {
+        height: 120,
+        backgroundColor: Colors.OXFORD_BLUE,
     },
     card: {
-        width: 320,
-        height: 470,
-        backgroundColor: '#FE474C',
-        borderRadius: 5,
+        width: normalize(360, 'width'),
+        borderRadius: 10,
         shadowColor: 'rgba(0,0,0,0.5)',
         shadowOffset: {
             width: 0,
             height: 1,
         },
         shadowOpacity: 0.5,
+        bottom: normalize(50, 'height'),
     },
-    card1: {
-        backgroundColor: '#FE474C',
-    },
-    card2: {
-        backgroundColor: '#FEB12C',
-    },
-    label: {
-        lineHeight: 400,
-        textAlign: 'center',
-        fontSize: 55,
-        fontFamily: 'System',
-        color: '#ffffff',
-        backgroundColor: 'transparent',
-    },
-    footer: {
-        flex: 1,
+    stretchBox: {
+        height: normalize(170, 'height'),
+        width: normalize(200, 'width'),
+        position: 'absolute',
+        top: normalize(40),
+        alignSelf: 'center',
+        borderRadius: 300,
+        borderWidth: 0.5,
+        borderColor: Colors.DUSTY_GRAY,
         justifyContent: 'center',
-        alignItems: 'center',
+        backgroundColor: Colors.CONCRETE,
     },
-    buttonContainer: {
-        width: 220,
+    stretch: {
+        height: '95%',
+        width: '95%',
+        borderRadius: 300,
+        alignSelf: 'center',
+    },
+    topBox: {
+        height: normalize(120, 'height'),
+        borderBottomWidth: 2,
+        borderColor: Colors.SILVER,
+        backgroundColor: Colors.ATHENS_GRAY,
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5,
+    },
+    bottomBox: {
+        height: normalize(450, 'height'),
+        backgroundColor: Colors.CONCRETE,
+    },
+    addressBox: {
+        top: normalize(120),
+        alignSelf: 'center',
+    },
+    addressTitle: {
+        fontSize: 25,
+        color: Colors.SILVER,
+        alignSelf: 'center',
+    },
+    addressContent: {
+        fontSize: 35,
+        color: Colors.OXFORD_BLUE,
+        alignSelf: 'center',
+    },
+    listFeature: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    button: {
-        shadowColor: 'rgba(0,0,0,0.3)',
-        shadowOffset: {
-            width: 0,
-            height: 1,
-        },
-        shadowOpacity: 0.5,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        zIndex: 0,
-    },
-    orange: {
-        width: 55,
-        height: 55,
-        borderWidth: 6,
-        borderColor: 'rgb(246,190,66)',
-        borderRadius: 55,
-        marginTop: -15,
-    },
-    green: {
-        width: 75,
-        height: 75,
-        backgroundColor: '#fff',
-        borderRadius: 75,
-        borderWidth: 6,
-        borderColor: '#01df8a',
-    },
-    red: {
-        width: 75,
-        height: 75,
-        backgroundColor: '#fff',
-        borderRadius: 75,
-        borderWidth: 6,
-        borderColor: '#fd267d',
     },
 });
